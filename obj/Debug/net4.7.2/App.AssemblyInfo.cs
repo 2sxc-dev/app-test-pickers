acd91828c3d2fb82f3816ab005012e67cbe85fca
Duje Vatavuk <duje@vatavuk.com>
2024-01-26 13:22:00 +0100

    minor add new tests to corespond with recent bugfixes

--- obj/Debug/net4.7.2/App.AssemblyInfo.cs
+++ obj/Debug/net4.7.2/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775a1e0ed7da9d0d548a19c6243e02991a12641a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57dc568f150c6577d5ae5765678e3b6b498cdf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
